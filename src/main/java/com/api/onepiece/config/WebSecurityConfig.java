687d04421a75a45d3c7d1a8759af2f934c61bed2
divaprates <divasilvaprates@gmail.com>
2023-10-15 21:49:27 -0300

    feat: spring security

---
+++ src/main/java/com/api/onepiece/config/WebSecurityConfig.java
@@ -0,0 +1,26 @@
+package com.api.onepiece.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig {
+
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user")
+                .password("{noop}user123")
+                .roles("USERS")
+                .and()
+                .withUser("admin")
+                .password("{noop}master123")
+                .roles("MANAGERS");
+    }
+}
